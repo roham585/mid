bf9421e23a9981b832181ce7c9014b05c1229bc4
roham585 <rohamnmty@gmail.com>
2024-07-12 19:46:23 +0330

    new

---
+++ js جاوا اسکریپ/java.js
@@ -0,0 +1,24 @@
+//var rtm="besmela";
+//alert( rtm);
+//document.write("hello"+rtm);
+//var r= prompt("نام خود را وارد کنید")
+//alert(r)
+//var t="12"
+//var yoo=+t
+//alert(typeof yoo)
+//var rt=prompt("vared kon")
+//var tr=prompt("vared kon")
+//var d=rt/ tr
+//alert(d)
+let button = document.getElementById("sh")
+let paragraph = document.getElementById("name")
+let cl = document.getElementById("lk")
+let tr="محتوا تغیر پیدا کرد"
+button.addEventListener('clik',function() {
+    paragraph.innerText='تغیر محتوا'
+    alert (tr);
+    }
+    )
+    
+   
+
